5bb489a484083dc36ffd465ea463bc1d71e50c0a
LS-plan <2016040219@s.upc.edu.cn>
2023-07-21 00:41:12 +0800

    简易可视化修改与3D图轴标签更新

--- static/js/myMap.js
+++ static/js/myMap.js
@@ -127,7 +127,6 @@
     [{ name: "西安" }, { name: "西宁", value: 100 }],
     [{ name: "西安" }, { name: "拉萨", value: 100 }]
   ];
-
   var XNData = [
     [{ name: "西宁" }, { name: "北京", value: 100 }],
     [{ name: "西宁" }, { name: "上海", value: 100 }],
@@ -135,12 +134,28 @@
     [{ name: "西宁" }, { name: "西安", value: 100 }],
     [{ name: "西宁" }, { name: "银川", value: 100 }]
   ];
-
   var YCData = [
     [{ name: "拉萨" }, { name: "北京", value: 100 }],
     [{ name: "拉萨" }, { name: "潍坊", value: 100 }],
     [{ name: "拉萨" }, { name: "哈尔滨", value: 100 }]
   ];
+
+  var NMGData = [
+    [{ name: "内蒙古" }, { name: "呼和浩特", value: 100 }],
+    [{ name: "内蒙古" }, { name: "乌鲁木齐", value: 100 }],
+    [{ name: "内蒙古" }, { name: "广州", value: 100 }],
+    [{ name: "内蒙古" }, { name: "西宁", value: 100 }],
+    [{ name: "内蒙古" }, { name: "拉萨", value: 100 }]
+  ];
+
+  // var XJData = [
+  //   [{ name: "新疆" }, { name: "北京", value: 100 }],
+  //   [{ name: "新疆" }, { name: "上海", value: 100 }],
+  //   [{ name: "新疆" }, { name: "广州", value: 100 }],
+  //   [{ name: "新疆" }, { name: "西宁", value: 100 }],
+  //   [{ name: "新疆" }, { name: "拉萨", value: 100 }]
+  // ];
+
 
   var planePath =
     "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z";
@@ -167,54 +182,57 @@
   var color = ["#fff", "#fff", "#fff"]; //航线的颜色
   var series = [];
   [
-    ["西安", XAData],
-    ["西宁", XNData],
-    ["银川", YCData]
+      ["内蒙古", NMGData],
+      // ["新疆", XJData]
+    // ["西安", XAData],
+    // ["西宁", XNData],
+    // ["银川", YCData]
+
   ].forEach(function(item, i) {
     series.push(
-      {
-        name: item[0] + " Top3",
-        type: "lines",
-        zlevel: 1,
-        effect: {
-          show: true,
-          period: 6,
-          trailLength: 0.7,
-          color: "red", //arrow箭头的颜色
-          symbolSize: 3
-        },
-        lineStyle: {
-          normal: {
-            color: color[i],
-            width: 0,
-            curveness: 0.2
-          }
-        },
-        data: convertData(item[1])
-      },
-      {
-        name: item[0] + " Top3",
-        type: "lines",
-        zlevel: 2,
-        symbol: ["none", "arrow"],
-        symbolSize: 10,
-        effect: {
-          show: true,
-          period: 6,
-          trailLength: 0,
-          symbol: planePath,
-          symbolSize: 15
-        },
-        lineStyle: {
-          normal: {
-            color: color[i],
-            width: 1,
-            opacity: 0.6,
-            curveness: 0.2
-          }
-        },
-        data: convertData(item[1])
-      },
+      // {
+      //   name: item[0] + " Top3",
+      //   type: "lines",
+      //   zlevel: 1,
+      //   effect: {
+      //     show: true,
+      //     period: 6,
+      //     trailLength: 0.7,
+      //     color: "red", //arrow箭头的颜色
+      //     symbolSize: 3
+      //   },
+      //   lineStyle: {
+      //     normal: {
+      //       color: color[i],
+      //       width: 0,
+      //       curveness: 0.2
+      //     }
+      //   },
+      //   data: convertData(item[1])
+      // },
+      // {
+      //   name: item[0] + " Top3",
+      //   type: "lines",
+      //   zlevel: 2,
+      //   symbol: ["none", "arrow"],
+      //   symbolSize: 10,
+      //   effect: {
+      //     show: true,
+      //     period: 6,
+      //     trailLength: 0,
+      //     symbol: planePath,
+      //     symbolSize: 15
+      //   },
+      //   lineStyle: {
+      //     normal: {
+      //       color: color[i],
+      //       width: 1,
+      //       opacity: 0.6,
+      //       curveness: 0.2
+      //     }
+      //   },
+      //   data: convertData(item[1])
+      // },
       {
         name: item[0] + " Top3",
         type: "effectScatter",
