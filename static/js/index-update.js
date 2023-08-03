b1c6f7b86cbb9d47f4a8b49405870c035e9dd724
LS-plan <2016040219@s.upc.edu.cn>
2023-08-03 15:51:13 +0800

    8月3日大屏异步2.1-初步正常

--- static/js/index-update.js
+++ static/js/index-update.js
@@ -1,16 +1,16 @@
-(await async function turbChange() {
+async function selectChange() {
     await main_update()
     await rose_update()
     await time_update()
     await length_update()
-})();
+};
 
 async function main_update(){
-
+    main()
 }
 
 async function rose_update(){
-
+    rose()
 }
 
 async function time_update(){
